1644e1e25ff214214e25ccc052bf2056a2f34478
Mikołaj Mrózek <m.mik311@gmail.com>
2022-04-10 23:34:43 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,16 +13,20 @@ module.exports = {
       },
       colors: {
         'bg': '#021931',
-        'accent': '#44fee0',
+        'accent': '#45FCDE',
         'header': '#ecedfc',
         'text': '#97aec5',
         'bg-element': '#1f3a56',
         'card': '#17223f',
-        'disabled': '#4a6682',
+        'disabled': '#18334F',
       },
       boxShadow: {
-        'start': 'inset 0 0 0 0 #44fee0',
-        'finish': 'inset 207px 0 0 0 #44fee0',
+        'start': 'inset 0 0 0 0.1px #45FCDE',
+        'finish': 'inset 350px 0 0 0.1px #45FCDE',
+        'finish2': 'inset 235px 0 0 0.1px #45FCDE',
+      },
+      screens: {
+        'specific': '1140px',
       }
     }
   },
