e42dc8bfee9f7c951aa8a99c9ee1055583373a2f
Mikołaj Mrózek <m.mik311@gmail.com>
2022-04-10 23:34:36 +0200

    Update FooterMobile.jsx

--- src/components/FooterMobile.jsx
+++ src/components/FooterMobile.jsx
@@ -7,13 +7,13 @@ export default function FooterMobile({ }) {
         </p>
         <div className="inline-flex my-auto">
             <a href="https://github.com/Reqill" target="_blank" className="group my-auto " title="GitHub">
-                <GitHub size={26} className="stroke-text stroke-[1.75] group-hover:stroke-accent transition-all ease-in-out " />
+                <GitHub size={26} className="stroke-text stroke-[1.5] group-hover:stroke-accent group-focus:stroke-accent transition-all ease-in-out " />
             </a>
             <a href="https://www.instagram.com/mmrozek.design/" target="_blank" className="group my-auto ml-3" title="Intagram">
-                <Instagram size={26} className="stroke-text stroke-[1.75] group-hover:stroke-accent transition-all ease-in-out " />
+                <Instagram size={26} className="stroke-text stroke-[1.5] group-hover:stroke-accent group-focus:stroke-accent transition-all ease-in-out " />
             </a>
             <a href="https://www.linkedin.com/in/miko%C5%82aj-mr%C3%B3zek-77b949214/" target="_blank" className="group my-auto ml-3" title="LinkedIn">
-                <Linkedin size={26} className="stroke-text stroke-[1.75] group-hover:stroke-accent transition-all ease-in-out " />
+                <Linkedin size={26} className="stroke-text stroke-[1.5] group-hover:stroke-accent group-focus:stroke-accent transition-all ease-in-out " />
             </a>
         </div>
     </div>;
