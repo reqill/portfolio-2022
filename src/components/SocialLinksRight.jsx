8d969448f784f6fb27c32626c76f5bee234238ac
Mikołaj Mrózek <m.mik311@gmail.com>
2022-04-15 14:48:24 +0200

    Revert "fixed social links animation"
    This reverts commit 38994900d1a2876b68bc99a629cfc7fa021d717c.

--- src/components/SocialLinksRight.jsx
+++ src/components/SocialLinksRight.jsx
@@ -1,20 +1,15 @@
 import React from "react";
 import { Instagram, Linkedin, GitHub } from 'react-feather';
-import Fade from 'react-reveal/Fade';
-
 export default function SocialLinksRight({ }) {
     return <div className="fixed right-4 md:right-6 lg:right-8 xl:right-12 top-[50vh] -translate-y-[50%] flex flex-col w-fit h-36 justify-between">
-        <Fade delay={1000}>
-            <a href="https://github.com/Reqill" target="_blank" className="group my-auto" title="GitHub">
-                <GitHub size={28} className="stroke-text stroke-[1.9] group-hover:stroke-accent group-hover:-translate-y-[.4rem] transition-all ease-in-out " />
-            </a>
-            <a href="https://www.instagram.com/mmrozek.design/" target="_blank" className="group my-auto" title="Intagram">
-                <Instagram size={28} className="stroke-text stroke-[1.9] group-hover:stroke-accent group-hover:-translate-y-[.4rem] transition-all ease-in-out " />
-            </a>
-            <a href="https://www.linkedin.com/in/miko%C5%82aj-mr%C3%B3zek-77b949214/" target="_blank" className="group my-auto" title="LinkedIn">
-                <Linkedin size={28} className="stroke-text stroke-[1.9] group-hover:stroke-accent group-hover:-translate-y-[.4rem] transition-all ease-in-out " />
-            </a>
-        </Fade>
-
+        <a href="https://github.com/Reqill" target="_blank" className="group my-auto" title="GitHub">
+            <GitHub size={28} className="stroke-text stroke-[1.9] group-hover:stroke-accent group-hover:-translate-y-[.4rem] transition-all ease-in-out " />
+        </a>
+        <a href="https://www.instagram.com/mmrozek.design/" target="_blank" className="group my-auto" title="Intagram">
+            <Instagram size={28} className="stroke-text stroke-[1.9] group-hover:stroke-accent group-hover:-translate-y-[.4rem] transition-all ease-in-out " />
+        </a>
+        <a href="https://www.linkedin.com/in/miko%C5%82aj-mr%C3%B3zek-77b949214/" target="_blank" className="group my-auto" title="LinkedIn">
+            <Linkedin size={28} className="stroke-text stroke-[1.9] group-hover:stroke-accent group-hover:-translate-y-[.4rem] transition-all ease-in-out " />
+        </a>
     </div>;
 }
